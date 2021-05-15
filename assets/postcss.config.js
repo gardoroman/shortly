2427d3910da83c58b655cc423dd850c55cffffcb
Edgardo Roman <eroman.transporttech.io>
2021-05-15 15:37:35 -0500

    Add tailwind css to project.

---
+++ assets/postcss.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//     plugins: {
+//       tailwindcss: {},
+//       autoprefixer: {},
+//     }
+//   }
+
+module.exports = {
+    plugins: [
+      require("tailwindcss"),
+      require("autoprefixer"),
+    ],
+  };
+  
